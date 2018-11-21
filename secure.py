ec47c191d0773fa7a419871eb3bd1a2927e3ee39
Valiant1011 <45211060+Valiant1011@users.noreply.github.com>
2018-11-21 15:41:49 +0530

    Minor tweaks

--- secure.py
+++ secure.py
@@ -1,9 +1,12 @@
 import string
 import random
+
 def new_password(size=6, chars=string.ascii_uppercase + string.digits+string.ascii_lowercase):
 	return ''.join(random.choice(chars) for _ in range(size)) #tuple comprehension
+
 def new_user(user_id):
 	user='team'+str(user_id)
 	return user
-send_id=raw_input('Enter sender Email ID:')
+
+sender_id=raw_input('Enter sender Email ID:')
 password=raw_input('Enter Email password:')
