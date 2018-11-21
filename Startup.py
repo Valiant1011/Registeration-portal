a186415bfb22736a33a54e15cdfe772e89e38caf
Valiant1011 <45211060+Valiant1011@users.noreply.github.com>
2018-11-21 16:21:50 +0530

    Startup.py now starts the server when the splash screen is hovered on.

--- Startup.py
+++ Startup.py
@@ -1,14 +1,11 @@
 from Tkinter import *
+from os import startfile
 splash=Tk()
 img=PhotoImage(file='C://Users//171b080//Desktop//splash.gif')
 def splash_end(event_mouse_moved):
     splash.destroy()
-
-    #call function
+    os.startfile(r'Server.py')
 event=Label(splash,image=img)
 event.bind('<Motion>',splash_end)
 event.pack()
 splash.mainloop()
-
-    
-    
