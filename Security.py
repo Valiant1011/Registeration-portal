4320c35e4e51acc52f7aaabbbc5ca0f60cb5ef76
Valiant1011 <45211060+Valiant1011@users.noreply.github.com>
2018-11-23 21:31:56 +0530

    Update Security.py

--- Security.py
+++ Security.py
@@ -4,10 +4,20 @@ import getpass
 
 def new_password(size=6, chars=string.ascii_uppercase + string.digits+string.ascii_lowercase):
 	return ''.join(random.choice(chars) for _ in range(size)) #tuple comprehension
-
 def new_user(user_id):
 	user='team'+str(user_id)
 	return user
 
+
 sender_id=raw_input('Enter sender Email ID:')
 password=getpass.getpass(prompt='Enter Email Password[ Will not be visible while typing ]:')
+
+
+
+
+
+
+
+
+
+
