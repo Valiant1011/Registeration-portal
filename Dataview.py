54bed7d1b43b5dd84ed8a416bc2465b7fa319c90
Valiant1011 <45211060+Valiant1011@users.noreply.github.com>
2018-12-22 18:21:56 +0530

    Delete Dataview.py

--- Dataview.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-con=sqlite3.connect("Database.db")
-cur=con.cursor()
-print 'Opened Database Successfully'
-cur.execute("select * from Records")
-x=cur.fetchall()
-print x
-con.close()
