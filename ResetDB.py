dd51a422f0602d069685b811852d4238ddafdf88
Valiant1011 <45211060+Valiant1011@users.noreply.github.com>
2018-12-22 18:21:34 +0530

    Delete ResetDB.py

--- ResetDB.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-con=sqlite3.connect("Database.db")
-cur=con.cursor()
-print 'Opened Database Successfully'
-cur.execute("drop table if exists Records")
-cur.execute("Create table Records (enrolment varchar2(30) PRIMARY KEY,email_id varchar2(50),username varchar2(10),password varchar2(7) ,UNIQUE(email_id))")
-print 'Created Table Successfully'
-con.close()
